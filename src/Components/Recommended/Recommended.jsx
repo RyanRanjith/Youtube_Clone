d57a611dc442c9e171394cb1b98d6ad49245961f
RyanRanjith <kingranjith4@gmail.com>
2024-04-04 08:32:16 -0700

    a

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -10,6 +10,7 @@ import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { API_KEY } from '../../data'
 import { value_converter } from '../../data'
+import { Link } from 'react-router-dom'
 
 
 
@@ -35,14 +36,14 @@ const Recommended = ({categoryId}) => {
     <div className='recommended'>
       {apiData.map((item,index)=>{
         return (
-          <div key={index} className="side-video-list">
+          <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className="side-video-list">
           <img src={item.snippet.thumbnails.medium.url} alt="" />
           <div className="vid-info">
              <h4>{item.snippet.title}</h4>
              <p>{item.snippet.channelTitle}</p>
              <p>{value_converter(item.statistics.viewCount)}</p>
           </div>
-         </div>
+         </Link>
         )
       })}
     </div>
